b9046c34e7b525ec71857a647a42640fbb6cc55a
nudommeas <measthannudom@gmail.com>
2024-10-14 11:09:12 +0700

    update

---
+++ DemoFirstProject/DemoFirstProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 100;
+            float pi = 3.14f;
+            double amount = 10000d;
+            string firstname = "Bro";
+            string lastname = "Sis";
+
+            int number2 = 500;
+            int result = number + number2;
+            //Console.Write(result);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
